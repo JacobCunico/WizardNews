eab9136eedafe9cefdcf08e614e36a9fddc2a1f4
Chris Farr <farsee37@gmail.com>
2023-05-03 22:24:25 -0500

    update

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.get("/", (req, res) => res.send(
       <body>
       <img src="/public/logo.png">
         <h1>Otters Frolicing</h1>
-        <img src="https://d1pk12b7bb81je.cloudfront.net/thumbor
-        /o_AXALnWng1WxIa0JzIyx09nLzA=/full-fit-in/800x800
-        /https://d1pk12b7bb81je.cloudfront.net/okdataengine/appmedia/images/69/69943/Sasquatch-forest.png">
+        <img src="public/download.jpg">
       </body>
   </html>`
 ));
