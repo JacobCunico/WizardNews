277385df87f723ec8bb07c5887d82abf351fe62c
Chris Farr <farsee37@gmail.com>
2023-05-03 20:34:33 -0500

    first run

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const PORT = 1337;
-console.log(fishj)
+
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
