75b4d97046b1a8311b54653aa5a2ab271ce88093
Chris Farr <farsee37@gmail.com>
2023-05-03 21:07:03 -0500

    added path for css and logo

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+
+app.use('/public', express.static(path.join(__dirname,'public')) )
 
 app.get("/", (req, res) => res.send(
   `<http>
      <head>
+     <link rel='stylesheet'href='public/style.css'/>
      </head>
-       <body>
-       <h1>Otters Frolicing</h1>
-       </body>
+      <body>
+      <img src="/public/logo.png">
+        <h1>Otters Frolicing</h1>
+      </body>
   </html>`
 ));
 
