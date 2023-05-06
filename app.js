83f2bbdbdf14179e68fcfd73bb63d8305a31bd7c
JacobCunico <Jacob.Cownico@gmail.com>
2023-05-06 15:01:28 -0600

    finshed project

--- app.js
+++ app.js
@@ -1,23 +1,75 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-
-app.use('/public', express.static(path.join(__dirname,'public')) )
-
-app.get("/", (req, res) => res.send(
-  `<http>
-     <head>
-     <link rel='stylesheet'href='public/style.css'/>
-     </head>
-      <body>
-      <img src="/public/logo.png">
-        <h1>Otters Frolicing</h1>
-        <img src="public/download.jpg">
-      </body>
-  </html>`
-));
-
+const postBank = require("./postBank");
 const PORT = 1337;
+
+app.use('/', express.static(path.join(__dirname,'public')) )
+
+app.get('/', (req, res,) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+      <a href="/posts/${post.id}">${post.title}</a>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+})
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id)
+  if (!post.id) {
+    throw new Error('Not Found')
+  }
+  res.send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News ${post.id}</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class='news-item'>
+        <p>
+          <span class="news-position">${post.id}. ▲</span>
+          ${post.title}
+          <small>(by ${post.name})</small>
+        </p>
+        <small class="news-info">
+          ${post.upvotes} upvotes | ${post.date}
+        </small>
+      </div>
+    </body>
+  </html>`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(404).send('404 Error: Page Not Found')
+})
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
